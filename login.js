45a8762cb836512dacca482a179e07b5f022cf09
Kevin Leon <kevinleon01@gmail.com>
2021-07-13 11:11:19 -0500

    ui cintinuity changes, password validation

--- login.js
+++ login.js
@@ -1,3 +1,10 @@
+//import json data
+var loginData = JSON.parse(data);
+// alert(loginData[0].name);
+// alert(loginData[0].age);
+// alert(loginData[1].name);
+// alert(loginData[1].age);
+
 // Log in info
 var username = document.getElementById("username")
 var password = document.getElementById("password")
@@ -9,6 +16,7 @@ const inputEmail = document.getElementById("email")
 var inputPassword = document.getElementById("password")
 var confirmPass = document.getElementById("confirm-password")
 
+//sign up function
 document.addEventListener('click', (event) => {
     if (event.target.id == 'sign-up') {
         localStorage.setItem("email", inputEmail.value)
@@ -17,6 +25,7 @@ document.addEventListener('click', (event) => {
     }
 })
 
+//log in function
 document.addEventListener('click', (event) => {
     if (event.target.id == 'log-in') {
         if ( username == username.value || username == email.value) {
