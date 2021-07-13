633078c3969993b9de22889e10d7a4ac9a35a8c9
Kevin Leon <kevinleon01@gmail.com>
2021-07-13 09:30:12 -0500

    login js added

---
+++ login.js
@@ -0,0 +1,17 @@
+// Log in info
+var username = document.getElementById("username")
+localStorage.setItem("email", username.value)
+var password = document.getElementById("password")
+localStorage.setItem("password", password.value)
+
+// sign up info
+var inputEmail = document.getElementById("email")
+localStorage.setItem("email", inputEmail.value)
+var inputPassword = document.getElementById("password")
+localStorage.setItem("password", password.value)
+var confirmPass = document.getElementById("confirm-password")
+localStorage.setItem("password", password.value)
+var inputName = document.getElementById("email")
+localStorage.setItem("email", inputEmail.value)
+var inputUsername = document.getElementById("password")
+localStorage.setItem("password", password.value)
