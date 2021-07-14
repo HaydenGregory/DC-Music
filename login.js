199d4aba3b1e587eb1f60dd484fc425d5df26288
Kevin Leon <kevinleon01@gmail.com>
2021-07-14 09:52:22 -0500

    deleted json file, reworked login js

--- login.js
+++ login.js
@@ -6,16 +6,26 @@ var loginData = JSON.parse(data);
 // alert(loginData[1].age);
 
 // Log in info
-var username = document.getElementById("username")
-var password = document.getElementById("password")
+var username = document.getElementById("login-username")
+var password = document.getElementById("login-password")
 
 
 // sign up info
-var inputUsername = document.getElementById("password")
+var inputUsername = document.getElementById("username")
 const inputEmail = document.getElementById("email")
 var inputPassword = document.getElementById("password")
-var confirmPass = document.getElementById("confirm-password")
+//var confirmPass = document.getElementById("confirm-password")
 
+//save sign up info
+function writeToFile(form) {
+
+    let fso = createObject("Scripting.FileSystemObject");
+    let s = fso.createTextFile("users.txt",True);
+
+    s.writeline("User: " + inputUsername +" "+inputEmail+" "+inputPassword)
+    s.writeline("------------")
+    s.Close();
+}
 //sign up function
 document.addEventListener('click', (event) => {
     if (event.target.id == 'sign-up') {
